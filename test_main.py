d4a3bed65f2c9d62c135eaad6c72a5480c27ed64
Max B. <maksym_borovyk@ukr.net>
2024-12-29 19:42:07 +0200

    Add locations copy test

--- test_main.py
+++ test_main.py
@@ -1,7 +1,7 @@
 import unittest
 import main
 
-class TestMain(unittest.TestCase):
+class TestLocation(unittest.TestCase):
     def test_locations_eq(self):
         """Test Location __eq__."""
         self.assertEqual(main.Location(1, 2),  main.Location(1, 2))
@@ -12,6 +12,14 @@ class TestMain(unittest.TestCase):
         """Test Location __eq__ exception for cases of different type."""
         with self.assertRaises(ValueError):
             _ = main.Location(0, 0) == (0, 0)
+
+    def test_locations_copy(self):
+        """Checks for Location().copy()"""
+        l1 = main.Location(1, 2)
+        l2 = l1.copy()
+        self.assertEqual(l1,  l2)
+        self.assertEqual(type(l1),  type(l1))
+        self.assertNotEqual(id(l1),  id(l2))
 
 if __name__ == "__main__":
     unittest.main()
