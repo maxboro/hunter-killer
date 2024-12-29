ccce57d8106e59d5e2c89047d342d75c4229f2b4
Max B. <maksym_borovyk@ukr.net>
2024-12-29 19:36:49 +0200

    Add test for Location exception

--- test_main.py
+++ test_main.py
@@ -3,9 +3,15 @@ import main
 
 class TestMain(unittest.TestCase):
     def test_locations_eq(self):
+        """Test Location __eq__."""
         self.assertEqual(main.Location(1, 2),  main.Location(1, 2))
         self.assertEqual(main.Location(0, 0),  main.Location(0, 0))
         self.assertNotEqual(main.Location(0, 0),  main.Location(1, 0))
+
+    def test_locations_eq_type_exeption(self):
+        """Test Location __eq__ exception for cases of different type."""
+        with self.assertRaises(ValueError):
+            _ = main.Location(0, 0) == (0, 0)
 
 if __name__ == "__main__":
     unittest.main()
