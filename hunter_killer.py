4d7f7f08cc728218aaac5f05e28ccb2e2c675b07
Max B. <maksym_borovyk@ukr.net>
2024-12-29 13:51:38 +0200

    Remove trailing whitespaces

--- hunter_killer.py
+++ hunter_killer.py
@@ -15,30 +15,30 @@ class MapBoundaries:
 
 
 class Location:
-    
+
     def __init__(self, x: int, y: int):
         self.x = x
         self.y = y
-    
+
     def __repr__(self):
         return f'[{self.x}; {self.y}]'
-    
+
     def copy(self):
         return Location(**self.__dict__)
-    
+
     def is_valid_location(self):
-        if (GLOBALS["MAP_BOUNDARIES"].x[0] <= self.x <= GLOBALS["MAP_BOUNDARIES"].x[1] 
+        if (GLOBALS["MAP_BOUNDARIES"].x[0] <= self.x <= GLOBALS["MAP_BOUNDARIES"].x[1]
             and GLOBALS["MAP_BOUNDARIES"].y[0] <= self.y <= GLOBALS["MAP_BOUNDARIES"].y[1]):
             return True
         else:
             return False
-        
-    
+
+
 class Randomizer:
-    
+
     def __init__(self, random_state):
         seed(random_state)
-        
+
     def create_random_location(self):
         x = randint(GLOBALS["MAP_BOUNDARIES"].x[0], GLOBALS["MAP_BOUNDARIES"].x[1])
         y = randint(GLOBALS["MAP_BOUNDARIES"].y[0], GLOBALS["MAP_BOUNDARIES"].y[1])
@@ -51,34 +51,34 @@ class Player:
         self._name = name
         self._location = location
         self._moves = None
-        
+
     def __repr__(self):
         return f'Player {self._name} at [{self._location.x}, {self._location.y}]'
-    
+
     @staticmethod
     def _move_left(new_potential_location):
         new_potential_location.x -= 1
         return new_potential_location
-    
+
     @staticmethod
     def _move_right(new_potential_location):
         new_potential_location.x += 1
         return new_potential_location
-    
+
     @staticmethod
     def _move_down(new_potential_location):
         new_potential_location.y -= 1
         return new_potential_location
-    
+
     @staticmethod
     def _move_up(new_potential_location):
         new_potential_location.y += 1
         return new_potential_location
-    
+
     @staticmethod
     def _move_pass(new_potential_location):
         return new_potential_location
-            
+
     def move(self, where: str):
         assert where in {'left', 'right', 'up', 'down', 'pass'}
         moves = {
@@ -93,73 +93,77 @@ class Player:
         new_potential_location = move_func(new_potential_location)
         if new_potential_location.is_valid_location:
             self._location = new_potential_location
-    
+
     def random_move(self):
         self.move(choice(self._moves))
 
 
 class Hunter(Player):
-    
+
     def __init__(self, location: Location):
         super().__init__(name="Hunter", location=location)
         self._moves = ['left', 'right', 'up', 'down']
-    
+
     def  __repr__(self):
         return f'Hunter at [{self._location.x}, {self._location.y}]'
 
 class Prey(Player):
-    
+
     def __init__(self, name: str, location: Location):
         super().__init__(name, location)
         self._moves = ['left', 'right', 'up', 'down'] + 3*['pass']
 
 
 class PreyList(UserList):
-    
+
     def random_move(self):
         for player in self:
             player.random_move()
-    
+
 class Game:
     """High level game management."""
     def __init__(self, randomizer):
         self._players = PreyList()
         self.hunter = None
         self._randomizer = randomizer
-    
+
     def add_player(self, player_name):
         new_player = Prey(
             name = player_name,
             location = self._randomizer.create_random_location()
         )
         self._players.append(new_player)
-        
+
     def add_hunter(self):
         self.hunter = Hunter(
             location = self._randomizer.create_random_location()
         )
-        
+
     def show_grid(self):
         for player in self._players:
             print(player)
         print(self.hunter)
-        
+
     def make_move(self):
         print('----------------------------')
         self.hunter.random_move()
         self._players.random_move()
-    
+
+
 def set_game(args: argparse.Namespace) -> Game:
-    GLOBALS["MAP_BOUNDARIES"] = MapBoundaries(x = (0, args.grid_size_x - 1), y = (0, args.grid_size_y - 1))
-    
+    GLOBALS["MAP_BOUNDARIES"] = MapBoundaries(
+        x = (0, args.grid_size_x - 1),
+        y = (0, args.grid_size_y - 1)
+    )
+
     randomizer = Randomizer(random_state = 1)
     game = Game(randomizer)
     for prey_id in range(args.n_prey):
         game.add_player(player_name = f'Prey_{prey_id}')
     game.add_hunter()
-    
+
     return game
-    
+
 def main(args: argparse.Namespace):
     print(f"Simulation parameters: {vars(args)}")
     game = set_game(args)
@@ -169,8 +173,7 @@ def main(args: argparse.Namespace):
         time.sleep(0.5)
         game.make_move()
         game.show_grid()
-    
-    
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='Hunter-killer',
